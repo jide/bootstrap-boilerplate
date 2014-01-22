665a00ae8f1c856922788f7c7dd31ec523cdc502
Julien De Luca <jide.fr@gmail.com>
2014-01-22 02:12:24 +0100

    Added minification, added a new grunt task to copy variables.less on the first launch

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,31 +10,60 @@ module.exports = function(grunt) {
       }
     },
     watch: {
+      options: {
+        atBegin: true
+      },
       less: {
         files: [
           'less/*.less',
           'bootstrap/less/*.less',
         ],
-        tasks: ['less']
+        tasks: ['copy','less'],
       },
     },
-    clean: {
-      dist: [
-        'styles/style.min.css'
-      ]
-    }
+    cssmin: {
+      compress: {
+        options: {
+          keepSpecialComments: '*',
+          noAdvanced: true, // turn advanced optimizations off until it's fixed in clean-css
+          report: 'min',
+          selectorsMergeMode: 'ie8'
+        },
+        src: 'css/style.css',
+        dest: 'css/style.min.css'
+      }
+    },
+    copy: {
+      variables: {
+        expand: true,
+        cwd: 'bootstrap/less',
+        src: 'variables.less',
+        dest: 'less',
+        filter: function(filepath) {
+          var path = require('path');
+          var dest = path.join(
+            grunt.config('copy.variables.dest'),
+            filepath.split(path.sep).slice(2).join(path.sep)
+          );
+          return !(grunt.file.exists(dest));
+        },
+        options: {
+          process: function (content, srcpath) {
+            return content.replace(/@icon-font-path:[\s\t]*"[^"]+"/g, '@icon-font-path: "../bootstrap/fonts/"');
+          }
+        }
+      },
+    },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   grunt.registerTask('default', [
-    'clean',
-    'less'
-  ]);
-
-  grunt.registerTask('dev', [
-    'watch'
+    'copy',
+    'less',
+    'cssmin'
   ]);
 };
--- README.md
+++ README.md
@@ -3,6 +3,11 @@ Bootstrap boilerplate
 
 Bootstrap boilerplate ready for customization. Override variables, mixins, classes and add your own style while keeping original Bootstrap files intact.
 
+First launch
+------------
+
+The first time you launch a grunt task, the variables.less file will be copied to the less folder, with the font path changed to be relative to the bootstrap folder. From then, you can modify variables.less to suit your needs.
+
 Usage
 -----
 
@@ -16,9 +21,4 @@ Usage
 
 **Production**
 
-    $ grunt less
-
-Todo
-----
-
-- Minify for production
+    $ grunt
